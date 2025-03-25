013f497fd54c5f030e2462bdad261225a0bc5577
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-03-25 20:04:01 +0100

    set - lesson5

---
+++ functions.py
@@ -0,0 +1,11 @@
+# def emriiFunksionit(parametri1, parametri2):
+#         print("Hello World")
+
+def helloworld():
+    print("Hello World")
+
+helloworld()
+
+
+def mbledhja(a,b):
+    rezultati = a + b
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 # loops - for
 
-fruits = ["apple", "banana", "watermelon", "strawberry"]
+# fruits = ["apple", "banana", "watermelon", "strawberry"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
 
-fjala = "Kimik"
+# fjala = "Kimik"
 
-for shkronje in fjala:
-    print(shkronje)
+# for shkronje in fjala:
+#     print(shkronje)
+
+# in range
+
+# for number in range(1,11):
+#     if number == 5:
+#         continue
+#     print(number)
+
+lista = [5,6,7,8,9,10,11,12,13]
+
+total_sum = 0
+
+for num in lista:
+    if num %2 ==0:
+        total_sum = total_sum + num
+print("Shuma totale e numrave cift eshte: ",total_sum)
